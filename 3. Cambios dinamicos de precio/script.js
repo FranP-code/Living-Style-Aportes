5f3758f2f49fd4a31c5374f8288c7e24a2a8f209
Francisco Pessano <franpessano1@gmail.com>
2022-06-27 13:28:48 -0300

    Cambio de precios added

---
+++ 3. Cambios dinamicos de precio/script.js
@@ -0,0 +1,93 @@
+function getPricesElements() {
+    const elements = document.querySelectorAll(".product-info .price span")
+    return elements
+}
+
+function main() {
+
+    //Get all options
+
+    const options = document.querySelectorAll("option")
+
+    //Get the option with (+$price) and select the father element
+    let select;
+
+    options.forEach(option => {
+
+        //If select defined, return
+        if (select) {
+            return
+        }
+
+        if (option.textContent.includes("(+$")) {
+            const number = option.textContent.substring(
+                option.textContent.indexOf("(+$") + 1, 
+                option.textContent.lastIndexOf(")")
+            );
+
+            if (number) {
+                select = option.parentElement
+            }
+        }
+    })
+
+    console.log(select)
+
+    //If there are more or less than one selected, report the error and stop the script
+    if (!select) {
+        console.error("ERROR SELECTING THE PRICE SELECT ELEMENT")
+        console.log("Select elements", selectElements)
+        console.log("Select element", select)
+        return
+    }
+
+    //Get prices of the product
+    const priceElements = [...getPricesElements()]
+
+    //Get the number of each price of the product
+    const priceElementsPrices = priceElements.map(priceElement => {
+        
+        const priceText = priceElement.innerText
+        
+        return parseInt(
+            priceText.substring(
+                priceText.indexOf("$") + 1,
+                priceText.length
+            ).replace(".","")
+        )
+    })
+
+    //Get shares element
+    const sharesElement = document.querySelector(".list-unstyled div p span b span span")
+
+    //Add event listener to the selected select element
+    select.addEventListener("change", (e) => {
+
+        //Get option text
+        const option = e.target.selectedOptions[0].innerText
+        
+        //Initialize the variable without increase on the price
+        let priceIncrease = 0
+
+        //If the text is a price, add it to the increase
+        if (option.includes("(+$")) {
+            let str = option.substring(
+                option.indexOf("(+$") + 3,
+                option.indexOf(")")
+            ).replace(".","")
+
+            priceIncrease = parseInt(str)
+        }
+        
+        //Replace the original price text with the actualizated price
+        priceElements.forEach((element, index) => {
+            element.innerText = `$${(priceElementsPrices[index] + priceIncrease).toLocaleString()}`
+        })
+
+        //Set the shares of the product
+        let sharesNumber = ((priceElementsPrices[0] + priceIncrease) / 10).toLocaleString(false, {minimumFractionDigits: 2})
+        sharesElement.innerHTML = `$ ${sharesNumber} <br>`
+    })
+}
+
+window.addEventListener('load', main) 
